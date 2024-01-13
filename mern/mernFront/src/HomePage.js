41ab049e915ccca82d1f394e9359e6e9ce64197e
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2024-01-14 02:35:47 +0600

    marging with the hosted site

--- mern/mernFront/src/HomePage.js
+++ mern/mernFront/src/HomePage.js
@@ -4,8 +4,8 @@ const HomePage = () => {
     return (
         <section className="home-container">
             <div className="home-content">
-                <h2 className="wow-title">Welcome to the Virtual Haven</h2>
-                <p className="wow-text">Get ready for an epic adventure in the world of Vertual Reality.</p>
+                <h2 className="wow-title">Welcome to the Virtual Heaven</h2>
+                <p className="wow-text">Get ready for an epic adventure in the world of Virtual Reality.</p>
             </div>
         </section>
     );
